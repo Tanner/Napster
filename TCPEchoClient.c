1525d73b6fe512bb540dd2a96b3caf7a0e04cbe0
Tanner Smith <tanner@tssoftware.net>
2012-09-21 13:26:34 -0400

    Clean up client code.

--- TCPEchoClient.c
+++ TCPEchoClient.c
@@ -1,74 +1,82 @@
-#include <stdio.h>      /* for printf() and fprintf() */
-#include <sys/socket.h> /* for socket(), connect(), send(), and recv() */
-#include <arpa/inet.h>  /* for sockaddr_in and inet_addr() */
-#include <stdlib.h>     /* for atoi() and exit() */
-#include <string.h>     /* for memset() */
-#include <unistd.h>     /* for close() */
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-#define RCVBUFSIZE 32   /* Size of receive buffer */
+#define RECEIVE_BUFFER_SIZE 32
 
-int main(int argc, char *argv[])
-{
-	int sock;                        /* Socket descriptor */
-	struct sockaddr_in echoServAddr; /* Echo server address */
-	unsigned short echoServPort;     /* Echo server port */
-	char *servIP;                    /* Server IP address (dotted quad) */
+#define DEFAULT_SERVER_PORT 7
+
+int main(int argc, char *argv[]) {
+	int sock;							// Socket Descriptor
+	struct sockaddr_in server_address;
+	unsigned short server_port;
+	char *server_ip;
 	char *echoString;                /* String to send to echo server */
-	char echoBuffer[RCVBUFSIZE];     /* Buffer for echo string */
+	char echoBuffer[RECEIVE_BUFFER_SIZE];     /* Buffer for echo string */
 	unsigned int echoStringLen;      /* Length of string to echo */
-	int bytesRcvd, totalBytesRcvd;   /* Bytes read in single recv() 
+	int bytes_received, total_bytes_received;   /* Bytes read in single recv() 
 										and total bytes read */
 
-	if ((argc < 3) || (argc > 4))    /* Test for correct number of arguments */
-	{
-	   fprintf(stderr, "Usage: %s <Server IP> <Echo Word> [<Echo Port>]\n",
-			   argv[0]);
+	if ((argc < 3) || (argc > 4)) {
+	   fprintf(stderr, "Usage: %s <Server IP> <Echo Word> [<Echo Port>]\n", argv[0]);
 	   exit(1);
 	}
 
-	servIP = argv[1];             /* First arg: server IP address (dotted quad) */
-	echoString = argv[2];         /* Second arg: string to echo */
+	server_ip = argv[1];	// First argument – Server IP (dotted quad)
+	echoString = argv[2];	// Second argument – String to echo
 
-	if (argc == 4)
-		echoServPort = atoi(argv[3]); /* Use given port, if any */
-	else
-		echoServPort = 7;  /* 7 is the well-known port for the echo service */
+	echoStringLen = strlen(echoString);
 
-	/* Create a reliable, stream socket using TCP */
-	if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
-		printf("socket() failed");
-
-	/* Construct the server address structure */
-	memset(&echoServAddr, 0, sizeof(echoServAddr));     /* Zero out structure */
-	echoServAddr.sin_family      = AF_INET;             /* Internet address family */
-	echoServAddr.sin_addr.s_addr = inet_addr(servIP);   /* Server IP address */
-	echoServAddr.sin_port        = htons(echoServPort); /* Server port */
-
-	/* Establish the connection to the echo server */
-	if (connect(sock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)
-		printf("connect() failed");
-
-	echoStringLen = strlen(echoString);          /* Determine input length */
-
-	/* Send the string to the server */
-	if (send(sock, echoString, echoStringLen, 0) != echoStringLen)
-		printf("send() sent a different number of bytes than expected");
-
-	/* Receive the same string back from the server */
-	totalBytesRcvd = 0;
-	printf("Received: ");                /* Setup to print the echoed string */
-	while (totalBytesRcvd < echoStringLen)
-	{
-		/* Receive up to the buffer size (minus 1 to leave space for
-		   a null terminator) bytes from the sender */
-		if ((bytesRcvd = recv(sock, echoBuffer, RCVBUFSIZE - 1, 0)) <= 0)
-			printf("recv() failed or connection closed prematurely");
-		totalBytesRcvd += bytesRcvd;   /* Keep tally of total bytes */
-		echoBuffer[bytesRcvd] = '\0';  /* Terminate the string! */
-		printf("%s", echoBuffer);      /* Print the echo buffer */
+	// Use a given port if one is provided, otherwise the default
+	if (argc == 4) {
+		server_port = atoi(argv[3]);
+	} else {
+		server_port = DEFAULT_SERVER_PORT;
 	}
 
-	printf("\n");    /* Print a final linefeed */
+	// Create a socket using TCP
+	if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+		fprintf(stderr, "Could not create socket.");
+		exit(2);
+	}
+
+	/* Construct the server address structure */
+	memset(&server_address, 0, sizeof(server_address));
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = inet_addr(server_ip);
+	server_address.sin_port = htons(server_port);
+
+	// Establish the connection to the server
+	if (connect(sock, (struct sockaddr *) &server_address, sizeof(server_address)) < 0) {
+		fprintf(stderr, "Could not connect to server.");
+		exit(2);
+	}
+
+	// Send the string to the server
+	if (send(sock, echoString, echoStringLen, 0) != echoStringLen) {
+		fprintf(stderr, "Sent a different number of bytes than expected.");
+		exit(2);
+	}
+
+	// Receive data from the server
+	printf("Received: ");
+
+	total_bytes_received = 0;
+	while (total_bytes_received < echoStringLen) {
+		if ((bytes_received = recv(sock, echoBuffer, RECEIVE_BUFFER_SIZE - 1, 0)) <= 0) {
+			fprintf(stderr, "Received failed or connection closed prematurely.");
+			exit(2);
+		}
+
+		total_bytes_received += bytes_received;
+		echoBuffer[bytes_received] = '\0';
+		printf("%s", echoBuffer);
+	}
+
+	printf("\n");
 
 	close(sock);
 	exit(0);
